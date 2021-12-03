f4c7ad63b0161b24b0bcc362989110c31f23582e
futabooo <takahiro.futagawa@gmail.com>
2021-12-04 00:00:37 +0900

    update local cli sample

--- example/local/README.md
+++ example/local/README.md
@@ -1,1 +1,11 @@
 A simple command-line application.
+
+Fetch
+```
+$ dart bin/local.dart fetch
+```
+
+Emit
+```
+$ dart bin/local.dart emit --signal=<IRSignal>
+```
--- example/local/bin/local.dart
+++ example/local/bin/local.dart
@@ -1,36 +1,109 @@
+import 'dart:io';
+
+import 'package:args/command_runner.dart';
 import 'package:multicast_dns/multicast_dns.dart';
 import 'package:nature_remo/nature_remo.dart';
 
 Future<void> main(List<String> arguments) async {
-  const String serviceType = '_remo._tcp';
-  late String target;
-  late String address;
+  exitCode = 0; // presume success
+  final runner = CommandRunner(
+    'nature_remo',
+    'A Nature Remo Local API Client that uses Nature API 1.0.0.',
+  )
+    ..addCommand(FetchCommand())
+    ..addCommand(EmitCommand());
 
-  final MDnsClient client = MDnsClient();
-  await client.start();
+  runner.run(arguments).catchError((error) {
+    print(error);
+    exitCode = 2;
+  });
+}
 
-  await for (final PtrResourceRecord ptr in client.lookup<PtrResourceRecord>(
-      ResourceRecordQuery.serverPointer(serviceType))) {
-    // Use the domainName from the PTR record to get the SRV record,
-    // which will have the port and local hostname.
-    // Note that duplicate messages may come through, especially if any
-    // other mDNS queries are running elsewhere on the machine.
-    await for (final SrvResourceRecord srv in client.lookup<SrvResourceRecord>(
-        ResourceRecordQuery.service(ptr.domainName))) {
-      print('target instance is${srv.target}');
-      target = srv.target;
+class FetchCommand extends Command with RemoFinder {
+  @override
+  final name = 'fetch';
+
+  @override
+  final description = 'Fetch the newest received IR signal';
+
+  FetchCommand() {}
+
+  @override
+  void run() async {
+    final address = await searchNatureRemo();
+    if (address == null) {
+      stderr.write('Nature Remo is not found');
+      exitCode = 2;
+      return;
     }
+    print(address);
+    final client = NatureRemoLocalApiClient(address: address);
+    final infraredSignal = await client.getMessages();
+    print('newest received IR signal ${infraredSignal.toJson()}');
+    stdout.writeln('newest received IR signal ${infraredSignal.toJson()}');
   }
+}
 
-  await for (final IPAddressResourceRecord record
-      in client.lookup<IPAddressResourceRecord>(
-          ResourceRecordQuery.addressIPv4(target))) {
-    print('Found address (${record.address}).');
-    address = record.address.address;
+class EmitCommand extends Command with RemoFinder {
+  @override
+  String get description => 'emit';
+
+  @override
+  String get name => 'Emit IR signals provided by request body';
+
+  EmitCommand() {
+    argParser.addOption('signal', abbr: 's', help: 'Infrared Signal');
   }
-  client.stop();
+  @override
+  void run() async {
+    final args = argResults;
+    if (args == null) {
+      stderr.write('signal option is required');
+      exitCode = 2;
+      return;
+    }
+    final infraredSignal = args['signal'];
+    final address = await searchNatureRemo();
+    if (address == null) {
+      stderr.write('Nature Remo is not found');
+      exitCode = 2;
+      return;
+    }
+    final client = NatureRemoLocalApiClient(address: address);
+    client.postMessages(infraredSignal: infraredSignal);
+  }
+}
 
-  final localApiClient = NatureRemoLocalApiClient(address: address);
-  final infraredSignal = await localApiClient.getMessages();
-  print('newest received IR signal ${infraredSignal.toJson()}');
+mixin RemoFinder {
+  Future<String?> searchNatureRemo() async {
+    const String serviceType = '_remo._tcp';
+    late String target;
+    late String address;
+
+    final MDnsClient client = MDnsClient();
+    await client.start();
+
+    await for (final PtrResourceRecord ptr in client.lookup<PtrResourceRecord>(
+        ResourceRecordQuery.serverPointer(serviceType))) {
+      // Use the domainName from the PTR record to get the SRV record,
+      // which will have the port and local hostname.
+      // Note that duplicate messages may come through, especially if any
+      // other mDNS queries are running elsewhere on the machine.
+      await for (final SrvResourceRecord srv
+          in client.lookup<SrvResourceRecord>(
+              ResourceRecordQuery.service(ptr.domainName))) {
+        print('target instance is${srv.target}');
+        target = srv.target;
+      }
+    }
+
+    await for (final IPAddressResourceRecord record
+        in client.lookup<IPAddressResourceRecord>(
+            ResourceRecordQuery.addressIPv4(target))) {
+      print('Found address (${record.address}).');
+      address = record.address.address;
+    }
+    client.stop();
+    return address;
+  }
 }
